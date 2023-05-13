4bd9abffdd361eb2549cf3d498da60fde7fef2f1
firttrule <vanezik23@mail.ru>
2023-05-13 20:50:12 +0300

    Выбор меша

--- src/scenes/MyFirstScene.js
+++ src/scenes/MyFirstScene.js
@@ -5,50 +5,95 @@ import {
   Vector3,
   MeshBuilder,
   HemisphericLight,
+  ActionManager,
+  InterpolateValueAction,
+  Color3,
+  SetValueAction,
+  AbstractMesh,
+  StandardMaterial,
+  PointerEventTypes,
 } from "babylonjs";
-
 
 const myScene = {
   engine: null,
   scene: null,
 
-createScene: function(canvas) {
-  const engine = new Engine(canvas);
-  const scene = new Scene(engine);
-  const camera = new ArcRotateCamera(
-    "camera",
-    -Math.PI / 2,
-    Math.PI / 2.5,
-    10,
-    new Vector3(0, 0, 0)
-  );
+  createScene: function (canvas) {
+    const engine = new Engine(canvas);
+    const scene = new Scene(engine);
 
-  camera.attachControl(canvas, true);
+    const camera = new ArcRotateCamera(
+      "camera",
+      -Math.PI / 2,
+      Math.PI / 2.5,
+      10,
+      new Vector3(0, 0, 0)
+    );
+    camera.attachControl(canvas, true);
 
-  const light = new HemisphericLight("light", new Vector3(0, 1, 0));
+    const light = new HemisphericLight("light", new Vector3(0, 1, 0));
+    light.intensity = 0.7;
 
-  light.intensity = 0.7;
+    let sphere = MeshBuilder.CreateSphere(
+      "sphere",
+      { diameter: 2, segments: 32 },
+      scene
+    );
+    sphere.position.y = 1;
+    sphere.material = new StandardMaterial("ground", scene);
 
-  const sphere = MeshBuilder.CreateSphere(
-    "sphere",
-    { diameter: 2, segments: 32 },
-    scene
-  );
+    const ground = MeshBuilder.CreateGround(
+      "ground",
+      { width: 6, height: 6 },
+      scene
+    );
 
-  sphere.position.y = 1;
+    let currentMesh;
+    const pointerDown = function (mesh) {
+      currentMesh = mesh;
+    };
 
-  const ground = MeshBuilder.CreateGround(
-    "ground",
-    { width: 6, height: 6 },
-    scene
-  );
+    scene.onPointerObservable.add((pointerInfo) => {
+      switch (pointerInfo.type) {
+        case PointerEventTypes.POINTERDOWN:
+          if (
+            pointerInfo.pickInfo.hit &&
+            pointerInfo.pickInfo.pickedMesh != ground
+          ) {
+            pointerDown(pointerInfo.pickInfo.pickedMesh);
+            pointerInfo.pickInfo.pickedMesh.material.diffuseColor =
+              Color3.Red();
+            currentMesh = pointerInfo.pickInfo.pickedMesh;
+          }
+          break;
+      }
+    });
 
-  engine.runRenderLoop(() => {
-    scene.render();
-  });
-}
+    // sphere.actionManager = new ActionManager(scene);
+    // sphere.actionManager
+    //   .registerAction(
+    //     new InterpolateValueAction(
+    //       ActionManager.OnLeftPickTrigger,
+    //       sphere,
+    //       "material.diffuseColor",
+    //       Color3.Red(),
+    //       1000
+    //     )
+    //   )
+    //   .then(
+    //     new InterpolateValueAction(
+    //       ActionManager.OnLeftPickTrigger,
+    //       sphere,
+    //       "material.diffuseColor",
+    //       Color3.Gray(),
+    //       1000
+    //     )
+    //   );
 
-}
-
+    engine.runRenderLoop(() => {
+      scene.render();
+    });
+  },
+};
 
 export { myScene };
