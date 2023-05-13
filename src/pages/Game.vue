949002679cff7af134974d1a4aa8a19c0f514b13
firttrule <vanezik23@mail.ru>
2023-05-13 13:16:49 +0300

    Отправка данных

--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -1,13 +1,16 @@
 <template>
-  <q-page class="flex flex-center">
-    <div>
+  <q-page class="q-ma-md">
+    <!-- <div>
       <q-radio dense v-model="btn" val="action" label="Курсор" />
       <q-radio dense v-model="btn" val="position" label="Смещение" />
       <q-radio dense v-model="btn" val="rotation" label="Вращение" />
-      <q-radio dense v-model="btn" val="scaling" label="Маштабирование" />
+      <q-radio dense v-model="btn" val="scaling" label="Масштабирование" />
+    </div> -->
+    <div>
+      <q-btn @click="moveCube">Move cube</q-btn>
     </div>
 
-    <Scene />
+    <Scene :position="cubePosition" />
   </q-page>
 </template>
 
@@ -16,4 +19,29 @@ import { ref, onMounted } from "@vue/runtime-core";
 import Scene from "../components/Scene.vue";
 
 const btn = ref("action");
+const cubePosition = ref({
+  x: 0,
+  y: 0,
+  z: 0,
+});
+const offset = ref(0);
+
+const moveCube = () => {
+  getNextPosition();
+  moveCubeTheRightWay();
+  console.log(cubePosition.value);
+};
+
+const moveCubeTheRightWay = () => {
+  cubePosition.value = {
+    x: 2,
+    y: cubePosition.value.y,
+    z: cubePosition.value.z,
+  };
+};
+
+const getNextPosition = () => {
+  offset.value += 0.5;
+  cubePosition.value = { x: 0, y: 0 + offset.value, z: 0 };
+};
 </script>
--- src/scenes/MyFirstScene.js
+++ src/scenes/MyFirstScene.js
@@ -10,6 +10,7 @@ import {
 const createScene = (canvas) => {
   const engine = new Engine(canvas);
   const scene = new Scene(engine);
+
   const camera = new ArcRotateCamera(
     "camera",
     -Math.PI / 2,
@@ -17,11 +18,9 @@ const createScene = (canvas) => {
     10,
     new Vector3(0, 0, 0)
   );
-
   camera.attachControl(canvas, true);
 
   const light = new HemisphericLight("light", new Vector3(0, 1, 0));
-
   light.intensity = 0.7;
 
   const sphere = MeshBuilder.CreateSphere(
@@ -29,7 +28,6 @@ const createScene = (canvas) => {
     { diameter: 2, segments: 32 },
     scene
   );
-
   sphere.position.y = 1;
 
   const ground = MeshBuilder.CreateGround(
@@ -43,4 +41,17 @@ const createScene = (canvas) => {
   });
 };
 
-export { createScene };
+const setPosition = (name, x, y, z) => {
+  const mesh = this.scene.getMeshByName(name)
+  if (mesh) {
+    mesh.position = new Vector3(x, y, z)
+  }
+}
+
+const myScene = {
+  engine: null,
+  scene: null,
+  createScene,
+  setPosition
+}
+export default myScene;
